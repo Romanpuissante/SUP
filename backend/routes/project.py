28ddf15d49f973270fdccddce4e022780ec4a3da
Vitaly <kuroedovo@narod.ru>
2021-12-20 18:01:37 +0300

    1111

--- backend/routes/project.py
+++ backend/routes/project.py
@@ -11,14 +11,16 @@ router = APIRouter(
     prefix='/project',
     tags=['Project'],
 )
+@router.get("/")
+async def get_all_projects():
+    pass
+# @router.post("/")
+# async def create_project(project: ProjectSchema):
+#     project_id = await Project.create(**project.dict())
+#     return {"project_id": project_id}
 
-@router.post("/")
-async def create_project(project: ProjectSchema):
-    project_id = await Project.create(**project.dict())
-    return {"project_id": project_id}
 
-
-@router.get("/{id}")
-async def get_project(id: int, ):
-    project = await Project.get_join(id, ("User",))
-    return {'project': project}
+# @router.get("/{id}")
+# async def get_project(id: int, ):
+#     project = await Project.get_join(id, ("User",))
+#     return {'project': project}
