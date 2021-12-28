502f2886a159fc6eb1a34f48b71d36ceacc7142f
Vitaly <kuroedovo@narod.ru>
2021-12-28 14:57:47 +0300

    цй

--- backend/routes/consumers.py
+++ backend/routes/consumers.py
@@ -37,80 +37,66 @@ router = APIRouter(
 #         async with broadcast.subscribe(channel=f"chatroom{user_id}") as subscriber:
 #             async for event in subscriber:
 #                 await websocket.send_text(event.message)
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections: list[WebSocket] = []
+# class ConnectionManager:
+#     def __init__(self):
+#         self.active_connections: list[WebSocket] = []
     
 
-    async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections.append(websocket)
+#     async def connect(self, websocket: WebSocket):
+#         await websocket.accept()
+#         self.active_connections.append(websocket)
         
 
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
+#     def disconnect(self, websocket: WebSocket):
+#         self.active_connections.remove(websocket)
 
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
+#     async def send_personal_message(self, message: str, websocket: WebSocket):
+#         await websocket.send_text(message)
 
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:           
-            await connection.send_text(message)
+#     async def broadcast(self, message: str):
+#         for connection in self.active_connections:           
+#             await connection.send_text(message)
 
 
-manager = ConnectionManager()
+# manager = ConnectionManager()
 
 @router.websocket("/")
-async def websocket(websocket: WebSocket, token: str = Query(...),Authorize: AuthJWT = Depends() ):
-    await manager.connect(websocket)
+async def websocketwork(websocket: WebSocket, token: str = Query(...),Authorize: AuthJWT = Depends() ):
+    await websocket.accept()
     # token: str = Query(...), Authorize: AuthJWT = Depends()   
     try:
         token = token.replace('Bearer ', '')
         Authorize.jwt_required("websocket", token=token)
         user_id = Authorize.get_raw_jwt(token)['user']['id']
         print(user_id)
-                
-        
-        
-        try:
-            
-            while True:
-                data = await websocket.receive_json(mode="text")
-                print(data)            
-                print("отправлено")
-            
-                pubsub = redis.pubsub()
-
-                
-                await pubsub.subscribe("channel:1", "channel:2")
-
-                future = asyncio.create_task(reader(pubsub, websocket))
-
-                await redis.publish("channel:1", json.dumps(data))
-                await redis.publish("channel:2", "World")
-                await redis.publish("channel:1", STOPWORD)
-
-                await future
-            
-                
-        except WebSocketDisconnect:
-            manager.disconnect(websocket)
-            await manager.broadcast(f"Client # left the chat")
+        pubsub = redis.pubsub()            
+        await pubsub.subscribe("channel:1", "channel:2")
     except AuthJWTException as err:
-        await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
-#     try:
+        await websocket.close(code=status.WS_1008_POLICY_VIOLATION)            
+        
+        
+    try:
+        
+        while True:
+            data = await websocket.receive_json(mode="text")
+            print(data)            
+            print("отправлено")
+        
+            
 
-#         token = token.replace('Bearer ', '')
-#         Authorize.jwt_required("websocket", token=token)
-#         user_id = Authorize.get_raw_jwt(token)['user']['id']
+            future = asyncio.create_task(reader(pubsub, websocket))
 
-#         await run_until_first_complete(
-#             (receiver, {"websocket": websocket, "user_id": user_id}),
-#             (sender, {"websocket": websocket, "user_id": user_id}),
-#         )
-#     except AuthJWTException as err:
-#         await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
+            await redis.publish("channel:1", json.dumps(data))
+            await redis.publish("channel:2", "World")
+            await redis.publish("channel:1", STOPWORD)
 
+            await future
+        
+            
+    except WebSocketDisconnect:
+        # manager.disconnect(websocket)
+        # await manager.broadcast(f"Client # left the chat")
+        print("foooooooooooooooooooooooooooooooooooooooooooo, disconnect")
     
 
 STOPWORD = "STOP"
@@ -122,12 +108,13 @@ async def reader(channel: aioredis.client.PubSub, websocket: WebSocket):
             async with async_timeout.timeout(1):
                 message = await channel.get_message(ignore_subscribe_messages=True)
                 if message is not None:
-                    await manager.send_personal_message(f"You wrote:", websocket)
-                    await manager.broadcast(f"Client #1 says: {message}")
-                    print(f"(Reader) Message Received: {message}")
                     if message["data"] == STOPWORD:
                         print("(Reader) STOP")
                         break
+                    # await manager.send_personal_message(f"You wrote:", websocket)
+                    await websocket.send_text(f"Client #1 says: {message}")
+                    print(f"(Reader) Message Received: {message}")
+                    
                 await asyncio.sleep(0.01)
         except asyncio.TimeoutError:
             pass
